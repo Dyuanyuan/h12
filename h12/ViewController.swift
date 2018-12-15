b9985cff12e11c7683b4655dc3ab96b940c228cc
student <student@imac-71.local>
2018-12-15 14:43:49 +0800

    Initial Commit

---
+++ h12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  h12
+//
+//  Created by student on 2018/12/15.
+//  Copyright © 2018年 heruili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
